3c6d901a448c489a40db82b51267e946e806324a
Andrey Vityuk <andrey.vityuk@gmail.com>
2013-02-14 23:13:57 -0800

    Created artifacts deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=0.2.2
+MODULES=(core servlet spring)
+
+for module in "${MODULES[@]}"
+do 
+   cd target/checkout/$module/target
+
+   mvn gpg:sign-and-deploy-file -Dgpg.passphrase= -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=ginger-$module-$VERSION.pom -Dfile=ginger-$module-$VERSION-sources.jar -Dclassifier=sources
+   mvn gpg:sign-and-deploy-file -Dgpg.passphrase= -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=ginger-$module-$VERSION.pom -Dfile=ginger-$module-$VERSION-javadoc.jar -Dclassifier=javadoc
+  
+   cd ../../../../
+done
+
